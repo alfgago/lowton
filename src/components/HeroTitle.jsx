8f7bf675cc965c89fc88a6b51b8943930bf8202f
Siwi <siwidjc@gmail.com>
2024-03-25 14:30:27 -0300

    added work

--- src/components/HeroTitle.jsx
+++ src/components/HeroTitle.jsx
@@ -6,7 +6,7 @@ export default function HeroTitle() {
     const [isActive, setIsActive] = useState(false);
 
     return (
-    <div>
+    <div className="hidden lg:flex">
         <h1 onMouseOver={() => {setIsActive(true)}} onMouseLeave={() => {setIsActive(false)}} className="flex flex-col text-[8vw] uppercase font-medium leading-tight text-white"><span>HI, Let's increase </span>the value of<span></span><span
               >your brand.</span></h1>
                 <Cursor isActive={isActive}/>
