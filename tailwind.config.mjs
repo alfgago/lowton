03b30b53212ab0ea14971f8284c9b9369bc9eb3a
Siwi <siwidjc@gmail.com>
2024-04-04 09:58:50 -0300

    finished desktop about page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      animation: {
+        "infinite-scroll": "infinite-scroll 25s linear infinite",
+        "infinite-scroll-reverse":
+          "infinite-scroll-reverse 25s linear infinite",
+      },
+      keyframes: {
+        "infinite-scroll": {
+          from: { transform: "translateX(0)" },
+          to: { transform: "translateX(calc(-270px * 4))" },
+        },
+        "infinite-scroll-reverse": {
+          from: { transform: "translateX(calc(-270px * 4))" },
+          to: { transform: "translateX(0)" },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
