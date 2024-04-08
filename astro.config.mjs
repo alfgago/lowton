9e340bab5b21b1b198b0bd8db22df92ae217533a
Siwi <siwidjc@gmail.com>
2024-04-08 10:10:09 -0300

    added SEO component on layout and updated site baseurl

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,18 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
+import { defineConfig } from "astro/config";
+import { seoConfig } from "./src/utils/seoConfig";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({
-    applyBaseStyles: false
-  }), react()],
-  output: "hybrid",
-  adapter: vercel()
+	integrations: [
+		tailwind({
+			applyBaseStyles: false,
+		}),
+		react(),
+	],
+	output: "hybrid",
+	adapter: vercel(),
+	site: seoConfig.baseURL,
 });
---
+++ src/utils/seoConfig.ts
@@ -0,0 +1,7 @@
+export const seoConfig = {
+	baseURL: "https://lowton.vercel.app/",
+	description:
+		"Experts in mixing the rational and the emotional to offer the most innovative and imaginative solutions to help your brand grow.",
+	type: "website",
+	siteName: "Lowton",
+};
