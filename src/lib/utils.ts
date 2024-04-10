a2d7bd6a37024baee5642be1a6aee429998a2a68
Siwi <siwidjc@gmail.com>
2024-04-10 10:16:57 -0300

    form contact synced, added lenis, fixed mobile view

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,4 @@
+import { gql } from "@apollo/client/core";
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
@@ -12,6 +13,131 @@ export function isEmpty(value: string | number | null | undefined) {
 export function hasContent(content: string | null | undefined): boolean {
 	if (!content) return false;  
   
-	// Check directly for empty <p></p>
-	return content.trim() !== '<p></p>'; 
+	const emptyParagraphPattern = /^\s*<p>\s*<\/p>\s*$/; 
+	return !emptyParagraphPattern.test(content); 
   }
+
+ export const SINGLE_PROJECT = gql`
+	query SingleProject($slug: String!) {
+		project(where: { slug: $slug }) {
+			name
+			slug
+			industry
+			videoUrl
+			year
+			client
+			shortIntro
+			aboutProject {
+				html
+			}
+			category {
+				... on Category {
+					id
+					name
+				}
+			}
+			components {
+				... on Video {
+					id
+					type
+					url
+				}
+				... on SingleImage {
+					id
+					type
+					image {
+						url
+						width
+						height
+						alt
+					}
+				}
+				... on Grid {
+					id
+					type
+					images {
+						url
+						width
+						height
+						alt
+					}
+					columns
+				}
+			}
+			featuredImage {
+				url
+				width
+				height
+				alt
+			}
+		}
+	}
+`;
+
+export const PROJECTS = gql`
+	query Projects {
+		projects {
+			name
+			slug
+			industry
+			videoUrl
+			year
+			client
+			shortIntro
+			aboutProject {
+				html
+			}
+			category {
+				... on Category {
+					id
+					name
+				}
+			}
+			components {
+				... on Video {
+					id
+					type
+					url
+				}
+				... on SingleImage {
+					id
+					type
+					image {
+						url
+						width
+						height
+						alt
+					}
+				}
+				... on Grid {
+					id
+					type
+					images {
+						url
+						width
+						height
+						alt
+					}
+					columns
+				}
+			}
+			featuredImage {
+				url
+				width
+				height
+				alt
+			}
+		}
+	}
+`;
+
+export const TESTIMONIALS = gql `
+query Testimonials {
+	testimonials {
+	  name
+	  text {
+		html
+	  }
+	}
+  }
+  `;
