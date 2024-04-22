27f2d41cbe78c209f50e478adde4e9db785ee361
Siwi <siwidjc@gmail.com>
2024-04-22 11:01:06 -0300

    adding robots txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,18 +4,17 @@ import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 import { defineConfig } from "astro/config";
 import { seoConfig } from "./src/utils/seoConfig";
+import sitemap from "@astrojs/sitemap";
 
-
+// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind({
     applyBaseStyles: false
-  }), react(),
-  partytown({
-	config: {
-	  forward: ["dataLayer.push"],
-	},
-  }),
-],
+  }), react(), partytown({
+    config: {
+      forward: ["dataLayer.push"]
+    }
+  }), sitemap()],
   output: "hybrid",
   image: {
     domains: ["us-east-1-shared-usea1-02.graphassets.com"]
